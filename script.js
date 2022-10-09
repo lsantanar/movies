1a891ff00d02bd20a04f825e12c7a179cd1fb24d
lsantanar <contatolsr@hotmail.com>
2022-10-09 11:47:23 -0300

    second adjustment

--- script.js
+++ script.js
@@ -9,27 +9,27 @@ const search = document.getElementById('btnHello');
 const movies = document.getElementById('btnMovies');
 const series = document.getElementById('btnSeries');
 
-// search.addEventListener ('click',  () => {
-//     let query = document.getElementById('searchBox').value
-//     fetch(`${ENDPOINT}/search/movie?api_key=${APIKEY}&query=${query}&language=pt-BR`)
-//        .then (res => res.json())
-//        .then (data => {
-//             let str = ''
-//             for (x=0; x< data.results.length; x++) {
-//                 let filme = data.results[x];
-//                 str += `<div class="card col-md-4 col-sm-6" width="300px">
-//                         <img src="${IMGENDPOINT}${filme.poster_path}" class="card-img-top" style="min-width: 200px" alt="...">
-//                         <div class="card-body">
-//                         <h5 class="card-title">${filme.title}</h5>
-//                         <p class="card-text">${filme.overview}</p>
-//                         <a href="${ABOUTENDPOINT}${filme.id}" class="btn btn-primary">Mais detalhes...</a>
-//                         </div>
-//                     </div>`
-//             }
-//             document.getElementById('tela').innerHTML = str
-//        } )
-//        .catch (err => console.log (`Deu ruim: ${err.message}`))
-// })
+search.addEventListener ('click',  () => {
+    let query = document.getElementById('searchBox').value
+    fetch(`${ENDPOINT}/search/movie?api_key=${APIKEY}&query=${query}&language=pt-BR`)
+       .then (res => res.json())
+       .then (data => {
+            let str = ''
+            for (x=0; x< data.results.length; x++) {
+                let filme = data.results[x];
+                str += `<div class="card col-md-4 col-sm-6" width="300px">
+                        <img src="${IMGENDPOINT}${filme.poster_path}" class="card-img-top" style="min-width: 200px" alt="...">
+                        <div class="card-body">
+                        <h5 class="card-title">${filme.title}</h5>
+                        <p class="card-text">${filme.overview}</p>
+                        <a href="${ABOUTENDPOINT}${filme.id}" class="btn btn-primary">Mais detalhes...</a>
+                        </div>
+                    </div>`
+            }
+            document.getElementById('tela').innerHTML = str
+       } )
+       .catch (err => console.log (`Deu ruim: ${err.message}`))
+})
 
 movies.addEventListener ('click',  () => {
     fetch(`${MOVIESENDPOINT}`)
@@ -51,5 +51,3 @@ movies.addEventListener ('click',  () => {
        } )
        .catch (err => console.log (`Deu ruim: ${err.message}`))
 })
-
-
